b77ab13caf4c36c854c084cfd4f431e532b7a1d0
Brandon <brantauber@Brandons-MacBook-Pro-2.local>
2014-02-28 10:22:11 -0500

    First Commit

---
+++ SuperCard2/PlayingCardView.m
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.m
+//  SuperCard2
+//
+//  Created by Brandon on 2/28/14.
+//  Copyright (c) 2014 Brandon Tauber. All rights reserved.
+//
+
+#import "PlayingCardView.h"
+
+@implementation PlayingCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
